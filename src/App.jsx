33eaf711ac77e9ea6b6979a7513bdca5c382fef8
AJP <a.pernito@aaconline.co.uk>
2022-11-07 13:14:20 +1100

    Necessary initials for react instance

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import './App.css';
+import React, { useState, createContext } from 'react';
+import ToggleTheme from './components/ToggleTheme';
+
+export const ThemeContext = createContext();
+
+function App() {
+
+  const themes = {
+    light: {
+      background: "#fff",
+      color: "#000",
+    },
+    dark: {
+      background: "#171717",
+      color: "#fff"
+    }
+  };
+
+  const [theme, setTheme] = useState('dark');
+  const toggleTheme = () => {
+    setTheme(theme === 'dark' ? 'light': 'dark');
+  }
+
+  const providerValue = {
+    details: {
+      name: theme,
+      styles: themes[theme],
+    },
+    toggleTheme,
+  }
+
+  return (
+    <>
+      <div className={`theme-${theme} todo-list-app`}>
+        <ThemeContext.Provider value={providerValue}>
+          <ToggleTheme/>
+          <div className="container">
+            <Main />
+          </div>
+        </ThemeContext.Provider>
+      </div>
+    </>
+  );
+}
+
+export default App;
